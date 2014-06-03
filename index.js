acc684a6be1c9df9a925e9a12120ac8fbdedd5a3
Andrew Shaw Care <andrewshawcare@gmail.com>
2014-06-03 14:37:09 -0400

    Added node-static and Heroku deployment configuration

---
+++ index.js
@@ -0,0 +1,11 @@
+/* global require, process */
+var NodeStatic = require('node-static'),
+    file = new NodeStatic.Server('./public'),
+    port = Number(process.env.PORT || 5000);
+
+require('http').createServer(function (request, response) {
+    "use strict";
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(port);
