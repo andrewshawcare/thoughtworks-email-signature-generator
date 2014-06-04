52f60219a221cf53305b1abf36a54c65c30ac06f
Andrew Shaw Care <andrewshawcare@gmail.com>
2014-06-03 23:39:50 -0400

    Ignored New Relic logging files Increased cache for static files from one hour to one year Added "Vary: Accept-Encoding" header to allow for compressed resources on proxy servers Removed meta charset tag from index and moved to HTTP header Added key to each select option to ensure proper state handling by React

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 /* global require, process */
 require("newrelic");
 var NodeStatic = require('node-static'),
-    file = new NodeStatic.Server('./public'),
+    file = new NodeStatic.Server('./public', {
+        cache: 60 * 60 * 24 * 365,
+        headers: {
+            "Vary": "Accept-Encoding"
+        }
+    }),
     port = Number(process.env.PORT || 5000);
 
 require('http').createServer(function (request, response) {
