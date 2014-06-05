509076ae88e9a1e48ff97e604fe727f4e04a7074
Andrew Shaw Care <andrewshawcare@gmail.com>
2014-06-05 11:15:31 -0400

    Fixed environment detection to delegate through Express.

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ require("newrelic");
 var Express = require("express"),
     Compression = require("compression"),
     port = Number(process.env.PORT || 5000),
-    staticDirectory = process.env.NODE_ENV === "production" ? "/build" : "/public",
+    server = Express(),
     maxAge = 1000 * 60 * 60 * 24 * 365,
-    server = Express();
+    directory = __dirname + (server.settings.env === "production" ? "/build" : "/public");
 
 server.use(Compression());
-server.use(Express.static(__dirname + staticDirectory, { maxAge: maxAge }));
+server.use(Express.static(directory, { maxAge: maxAge }));
 
 server.listen(port);
