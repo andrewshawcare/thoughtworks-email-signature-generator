3e87e0371c945cadb426abd7d2cfb4a7c48394bd
Andrew Shaw Care <andrewshawcare@gmail.com>
2014-06-09 19:58:58 -0400

    Reduced cache to one day from one year

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Express = require("express"),
     Compression = require("compression"),
     port = Number(process.env.PORT || 5000),
     server = Express(),
-    maxAge = 1000 * 60 * 60 * 24 * 365,
+    maxAge = 1000 * 60 * 60 * 24, // 1,000 milliseconds/second * 60 seconds/minute * 60 minutes/hour * 24 hours/day = 1 day
     directory = __dirname + (server.settings.env === "production" ? "/build" : "/public");
 
 server.use(Compression());
