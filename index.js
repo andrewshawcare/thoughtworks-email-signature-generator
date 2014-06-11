84058bb948f770c00973245c8e1ed6b39a3fe1b0
Andrew Shaw Care <andrewshawcare@gmail.com>
2014-06-11 11:49:28 -0400

    Changed caching from one day to one year

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ var Express = require("express"),
     Compression = require("compression"),
     port = Number(process.env.PORT || 5000),
     server = Express(),
-    maxAge = 1000 * 60 * 60 * 24, // 1,000 milliseconds/second * 60 seconds/minute * 60 minutes/hour * 24 hours/day = 1 day
+    /*
+        1,000 milliseconds/second *
+        60 seconds/minute *
+        60 minutes/hour *
+        24 hours/day *
+        365 days/year =
+        1 year
+    */
+    maxAge = 1000 * 60 * 60 * 24 * 365,
     directory = __dirname + (server.settings.env === "production" ? "/build" : "/public");
 
 server.use(Compression());
