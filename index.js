e50ed258ef8e383d4999cd0b84cba2bc8678d4eb
Andrew Shaw Care <andrewshawcare@gmail.com>
2014-06-12 11:54:27 -0400

    Added Jade templating to allow for New Relic real-user monitoring

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /* global require, process, __dirname */
 require("newrelic");
-var Express = require("express"),
+var NewRelic = require("newrelic"),
+    Express = require("express"),
     Compression = require("compression"),
+    Jade = require("jade"),
     port = Number(process.env.PORT || 5000),
     server = new Express(),
     /*
@@ -15,6 +17,13 @@ var Express = require("express"),
     maxAge = 1000 * 60 * 60 * 24 * 365,
     directory = __dirname + (server.settings.env === "production" ? "/build" : "/public");
 
+server.locals.newrelic = NewRelic;
+server.set("views", __dirname + "/views");
+server.engine("jade", Jade.__express);
+server.get("/", function (request, response) {
+    "use strict";
+    response.render("index.jade");
+});
 server.use(new Compression());
 server.use(Express.static(directory, { maxAge: maxAge }));
 
