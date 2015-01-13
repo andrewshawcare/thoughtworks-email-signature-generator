9747ed84cbfafa04c9c65416fe0ee44b674a80f9
Andrew Shaw Care <andrewshawcare@users.noreply.github.com>
2015-01-13 13:33:14 -0500

    Hope to enable gzip by introducing earlier.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /* global require, process, __dirname, console */
 require("newrelic");
-var NewRelic = require("newrelic"),
-    Express = require("express"),
+var Express = require("express"),
+    newrelic = require("newrelic"),
     compression = require("compression"),
-    Jade = require("jade"),
+    jade = require("jade"),
     port = Number(process.env.PORT || 5000),
     server = new Express(),
     /*
@@ -17,11 +17,13 @@ var NewRelic = require("newrelic"),
     maxAge = 1000 * 60 * 60 * 24 * 365,
     directory = __dirname + (server.settings.env === "production" ? "/build" : "/public");
 
-server.locals.newrelic = NewRelic;
-server.set("views", __dirname + "/views");
-server.engine("jade", Jade.__express);
+server.locals.newrelic = newrelic;
+
 server.use(compression());
 server.use(Express.static(directory, { maxAge: maxAge }));
+
+server.set("views", __dirname + "/views");
+server.engine("jade", jade.__express);
 
 server.get("/", function (request, response) {
     "use strict";
