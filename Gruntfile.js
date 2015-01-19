acccc77d7b350d4f03bc03c26eb6fa65b0d564cf
Andrew Shaw Care <andrewshawcare@gmail.com>
2015-01-18 23:57:56 -0500

    Decoupled task and aggregate task registration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,23 @@
 /* global module, require */
 module.exports = function (grunt) {
     "use strict";
-    var fs = require("fs"),
-        tasksDirectory = "./tasks/",
-        taskNames = fs.readdirSync(tasksDirectory).filter(function (filename) {
+    var tasksDirectory = "./tasks/";
+    
+    // register tasks
+    require("fs").readdirSync(tasksDirectory)
+        .filter(function (filename) {
             // filter non-requireable files
             return /.*\.(js|json)$/.test(filename);
-        }),
-        aggregateTasks = require(tasksDirectory + "data/aggregate_tasks");
+        })
+        .forEach(function (taskName) {
+            var task = require(tasksDirectory + taskName);
+            grunt.loadNpmTasks(task.name);
+            grunt.config.set(task.id, task.configuration);
+        });
 
-    taskNames.forEach(function (taskName) {
-        var task = require(tasksDirectory + taskName);
-        grunt.loadNpmTasks(task.name);
-        grunt.config.set(task.id, task.configuration);
-    });
-
-    aggregateTasks.forEach(function (aggregateTask) {
-        grunt.registerTask(aggregateTask.name, aggregateTask.tasks);
-    });
+    // register aggregate tasks
+    require(tasksDirectory + "data/aggregate_tasks")
+        .forEach(function (aggregateTask) {
+            grunt.registerTask(aggregateTask.name, aggregateTask.tasks);
+        });
 };
