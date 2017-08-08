7c4bc2d31c3ae6de618bec8923e07d7e6b50ec3a
Nic Smythe <nicsmythe@nsmytheAUC02P9101G3QR.local>
2017-08-09 08:05:13 +1000

    Pride localisations

--- public/javascript/properties.js
+++ public/javascript/properties.js
@@ -179,7 +179,7 @@ define([], function () {
             "orange": "Orange",
             "pink": "Pink",
             "plum": "Pflaume",
-            "pride": "Pride",
+            "pride": "Stolz",
             "red": "Rot",
             "warmth": "Warm",
             "previewLabel": "Vorschau",
